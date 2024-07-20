29e4aadd8ef38a89cfc86cf1e969378be21cafb4
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2024-07-19 21:00:37 -0500

    refactor: Update SourceBox component to include link prop

--- src/components/source-box.tsx
+++ src/components/source-box.tsx
@@ -1,19 +1,22 @@
 import { Link } from "lucide-react";
 
 interface SourceBoxProps {
-  title: string;
-  label: string;
+  title?: string;
+  label?: string;
+  link?: string;
 }
 
-const SourceBox = ({ title, label }: SourceBoxProps) => {
+const SourceBox = ({ title, label, link }: SourceBoxProps) => {
   return (
-    <article className="max-w-[450px] w-full cursor-pointer bg-gray-200/45 px-5 pb-5 py-14 rounded-lg hover:bg-gray-200/70 transition-all duration-100 ease-in-out">
-      <h2 className="text-[18px] uppercase text-neutral-700 line-clamp-3 text-balance">{title}</h2>
-      <div className="flex items-center gap-2">
-        <Link className="size-[14px] text-neutral-700" />
-        <h3 className="text-[12px] uppercase font-bold text-neutral-700 line-clamp-1">{label}</h3>
-      </div>
-    </article>
+    <a href={link} target="_blank" className="flex flex-col justify-end w-full cursor-pointer bg-gray-200/45 px-5 pb-5 py-14 rounded-lg hover:bg-gray-200/70 transition-all duration-100 ease-in-out">
+      <article className="">
+        <h2 className="text-[18px] leading-[22.8px]  font-bold mb-2 uppercase text-neutral-700 line-clamp-3 text-balance">{title}</h2>
+        <div className="w-full flex items-center gap-2">
+          <Link className="size-[14px] text-neutral-700 min-w-[14px]" />
+          <h3 className="text-[12px] uppercase text-neutral-700 font-medium line-clamp-1">{label}</h3>
+        </div>
+      </article>
+    </a>
   );
 };
 
