0d0805eeb1d1d8b4aa4c041896e021d00ced7765
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2024-08-21 22:13:13 -0500

    refactor: Update sheet settings component to improve user instructions and input labels

--- src/components/sheet-settings.tsx
+++ src/components/sheet-settings.tsx
@@ -8,19 +8,25 @@ const SheetSettings = () => {
 
       <div className="group-fields">
         <Label>Selecciona el modelo</Label>
-        <p>Elige el modelo que deseas utilizar para generar el JSON. Necesitas añadir la API KEY correspondiente al model seleccionado para poder utilizarlo.</p>
+        <p>
+          Elige el modelo de IA que deseas utilizar para la generación de la respuesta.
+        </p>
         <Input type="text" />
       </div>
 
       <div className="group-fields">
         <Label>Selecciona el nivel de creatividad</Label>
-        <p>Elige el nivel de creatividad que deseas que tenga el modelo para generar el JSON.</p>
+        <p>
+          Moldea la creatividad de la respuesta. Un nivel bajo generará respuestas más predecibles, mientras que un nivel alto generará respuestas más inesperadas.
+        </p>
         <Input type="text" />
       </div>
 
       <div className="group-fields">
         <Label>Ingresa tu API KEY</Label>
-        <p>Ingresa la API KEY correspondiente al modelo seleccionado para poder utilizarlo y generar el JSON.</p>
+        <p>
+          Ingresa tu clave de API para poder utilizar los servicios del modelo que seleccionaste.
+        </p>
         <Input type="text" />
       </div>
 
